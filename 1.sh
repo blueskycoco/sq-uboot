e9c254616479711df118d897776f55a133c615b6
dillonhua <dillonhua@qq.com>
2014-02-19 20:03:34 -0800

    update sh and gitignore

--- 1.sh
+++ 1.sh
@@ -1,7 +1,7 @@
 #! /bin/bash -e
 
-export CROSS_COMPILE=arm-none-linux-gnueabi-
-export PATH=$PATH:/home/rdp/work/ti/am3359/tools/
+export CROSS_COMPILE=arm-linux-gnueabihf-
+export PATH=$PATH:/home/rdp/bin/gcc-linaro-arm-linux-gnueabihf-4.7-2013.03-20130313_linux/bin
 export ARCH=arm
 export PATH=$PATH:/home/rdp/work/ti/am3359/tools/arm-2009q1/bin
 
